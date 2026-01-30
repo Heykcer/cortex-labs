9c1005c1fcadc6f961632a0a478dcfeaa4849e4f
saquib <rayeessaquib0022@gmail.com>
2026-01-30 14:57:52 +0530

    checked nvbar edit

--- client/src/Dashboard/Navbar.jsx
+++ client/src/Dashboard/Navbar.jsx
@@ -12,9 +12,9 @@ const Navbar = () => {
 
                 <ul className="nav-links">
                     <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#events">Events</a></li>
-                    <li><a href="#about">About</a></li>
                     <li><a href="#contact" className="join-btn">Join Us</a></li>
 
                 </ul>
