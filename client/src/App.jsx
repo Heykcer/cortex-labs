2c54c9e58bfdbf6dcc7ba59d4337fd1847a705d6
Heykcer <mdtanjilalam69@gmail.com>
2026-01-30 12:33:34 +0530

    reduced hard coding,separated the components and landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import logoImg from "./assets/logo2.png";
-
+import Navbar from "./Dashboard/Navbar";
 function App() {
   return (
     <>
       <header className="header">
-        <nav className="navbar">
-          <div className="logo">
-            <img src={logoImg} alt="Logo" />
-          </div>
-
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <main>
